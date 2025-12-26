1396cb631db1d1afb586135c864eab76a13e2ff6
Garrett-Floyd <Garrett.Floyd.Github@gmail.com>
2025-12-26 00:55:59 -0500

    first commit

---
+++ _build/assets/about-58lPHZru.js
@@ -0,0 +1,1 @@
+import{t as e,A as a,j as r,w as n,T as o}from"./index-BxTDfHai.js";var l=n("<main>");function s(){return(()=>{var t=e(l);return a(t,r(o,{children:"About"})),t})()}export{s as default};
